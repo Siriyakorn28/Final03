2034cd9516bd3b1c548267657befb30c368d38b8
Siriyakorn28 <77780821+Siriyakorn28@users.noreply.github.com>
2021-03-31 17:14:16 +0700

    03

---
+++ app/src/main/java/com/example/final03/Quiz05.kt
@@ -0,0 +1,33 @@
+package com.example.final03
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_quiz01.*
+
+class Quiz05 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz05)
+
+        val text01 = Toast.makeText(applicationContext,"Correct", Toast.LENGTH_LONG)
+        val text02 = Toast.makeText(applicationContext,"Incorrect", Toast.LENGTH_LONG)
+        val text03 = Toast.makeText(applicationContext,"Delete complete", Toast.LENGTH_LONG)
+
+        btnnext.setOnClickListener{
+            if (this.radioButton2.text.toString() == ""){
+                text01.show()
+            }else{
+                text02.show()
+                startActivity(Intent(this,Quiz02::class.java))
+            }
+        }
+        btnclear.setOnClickListener{
+            radioButton1.setText(null)
+            radioButton2.setText(null)
+            radioButton3.setText(null)
+            text03.show()
+        }
+    }
+}
